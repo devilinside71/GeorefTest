1c4337df943614898e72cf0eeeb91670a9c6315c
devilinside71 <laszlo.devil@gmail.com>
2022-07-19 17:14:44 +0200

    georef2DEG finished (4 number)

--- src/main/kotlin/Georef.kt
+++ src/main/kotlin/Georef.kt
@@ -34,12 +34,111 @@ class Georef {
 //            }
             var mLength = matcher.group(5).length
             if (mLength % 2 == 0) {
-                println("OK")
+                var lonMulti = 1
+                var latMulti = 1
+                val lon1 = matcher.group(1)
+                var lon1Val = georef15IDVal(lon1)
+                println("georef2DMS lon1Val $lon1Val")
+                val lat1 = matcher.group(2)
+                var lat1Val = georef15IDVal(lat1)
+                println("georef2DMS lat1Val $lat1Val")
+                val lon2 = matcher.group(3)
+                val lon2Val = georef1515IDVal(lon2)
+                println("georef2DMS lon2Val $lon2Val")
+                val lat2 = matcher.group(4)
+                val lat2Val = georef1515IDVal(lat2)
+                println("georef2DMS lat2Val $lat2Val")
+                if (lon1Val < 180) {
+                    lon1Val = 180 - lon1Val
+                    lonMulti = -1
+                } else {
+                    lon1Val -= 180
+                    lonMulti = 1
+                }
+                println("georef2DMS lon1Val mod $lon1Val")
+                lon1Val = lon1Val + lonMulti * lon2Val
+                println("georef2DMS lon1Val mod2 $lon1Val")
+                if (lat1Val < 90) {
+                    lat1Val = 90 - lat1Val
+                    latMulti = -1
+                } else {
+                    lat1Val -= 90
+                    latMulti = 1
+                }
+                println("georef2DMS lat1Val mod $lat1Val")
+                lat1Val = lat1Val + latMulti * lat2Val
+                println("georef2DMS lat1Val mod2 $lat1Val")
+
+                if (mLength == 4) {
+                    var nums = matcher.group(5)
+                    var lon3 = nums.substring(0, 2)
+                    var lat3 = nums.substring(2, 4)
+                    println("georef2DMS nums $lon3 $lat3")
+                    var lon4 = lon1Val.toDouble() + lonMulti * (lon3.toDouble() / 60.0)
+                    var lat4 = lat1Val.toDouble() + latMulti * (lat3.toDouble() / 60.0)
+                    lon4 = lonMulti * lon4
+                    lat4 = latMulti * lat4
+                    println("georef2DMS nums2 $lon4 $lat4")
+                    retVal.Latitude = lat4
+                    retVal.Longitude = lon4
+                }
             }
         }
-        println("georef2DEG - RETURN $retVal")
+        println("georef2DMS - RETURN $retVal")
         return retVal
 
+    }
+
+    fun georef1515IDVal(coord: String): Int {
+        val letters: MutableMap<String, Int> = HashMap()
+        letters["A"] = 0
+        letters["B"] = 1
+        letters["C"] = 2
+        letters["D"] = 3
+        letters["E"] = 4
+        letters["F"] = 5
+        letters["G"] = 6
+        letters["H"] = 7
+        letters["J"] = 8
+        letters["K"] = 9
+        letters["L"] = 10
+        letters["M"] = 11
+        letters["N"] = 12
+        letters["P"] = 13
+        letters["Q"] = 14
+        val retVal = letters[coord]!!
+        return retVal
+    }
+
+    fun georef15IDVal(coord: String): Int {
+
+        val letters: MutableMap<String, Int> = HashMap()
+        letters["A"] = 0
+        letters["B"] = 15
+        letters["C"] = 30
+        letters["D"] = 45
+        letters["E"] = 60
+        letters["F"] = 75
+        letters["G"] = 90
+        letters["H"] = 105
+        letters["J"] = 120
+        letters["K"] = 135
+        letters["L"] = 150
+        letters["M"] = 165
+        letters["N"] = 180
+        letters["P"] = 195
+        letters["Q"] = 210
+        letters["R"] = 225
+        letters["S"] = 240
+        letters["T"] = 255
+        letters["U"] = 270
+        letters["V"] = 285
+        letters["W"] = 300
+        letters["X"] = 315
+        letters["Y"] = 330
+        letters["Z"] = 345
+        val retVal = letters[coord]!!
+        return retVal
     }
 
     fun georefLon15(lonDeg: Int, lonHem: String): String {
