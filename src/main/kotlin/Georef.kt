cc57f9b2230708837daf58cf2ccf9cb3934b4f63
devilinside71 <laszlo.devil@gmail.com>
2022-07-20 10:20:13 +0200

    Bugfixes

--- src/main/kotlin/Georef.kt
+++ src/main/kotlin/Georef.kt
@@ -42,16 +42,16 @@ class Georef {
                 var latMulti = 1
                 val lon1 = matcher.group(1)
                 var lon1Val = georef15IDVal(lon1)
-                println("georef2DMS lon1Val $lon1Val")
+                println("georef2DEG lon1Val $lon1Val")
                 val lat1 = matcher.group(2)
                 var lat1Val = georef15IDVal(lat1)
-                println("georef2DMS lat1Val $lat1Val")
+                println("georef2DEG lat1Val $lat1Val")
                 val lon2 = matcher.group(3)
                 val lon2Val = georef1515IDVal(lon2)
-                println("georef2DMS lon2Val $lon2Val")
+                println("georef2DEG lon2Val $lon2Val")
                 val lat2 = matcher.group(4)
                 val lat2Val = georef1515IDVal(lat2)
-                println("georef2DMS lat2Val $lat2Val")
+                println("georef2DEG lat2Val $lat2Val")
                 if (lon1Val < 180) {
                     lon1Val = 180 - lon1Val
                     lonMulti = -1
@@ -59,9 +59,9 @@ class Georef {
                     lon1Val -= 180
                     lonMulti = 1
                 }
-                println("georef2DMS lon1Val mod $lon1Val")
+                println("georef2DEG lon1Val mod $lon1Val")
                 lon1Val += lonMulti * lon2Val
-                println("georef2DMS lon1Val mod2 $lon1Val")
+                println("georef2DEG lon1Val mod2 $lon1Val")
                 if (lat1Val < 90) {
                     lat1Val = 90 - lat1Val
                     latMulti = -1
@@ -69,24 +69,24 @@ class Georef {
                     lat1Val -= 90
                     latMulti = 1
                 }
-                println("georef2DMS lat1Val mod $lat1Val")
+                println("georef2DEG lat1Val mod $lat1Val")
                 lat1Val += latMulti * lat2Val
-                println("georef2DMS lat1Val mod2 $lat1Val")
+                println("georef2DEG lat1Val mod2 $lat1Val")
                 var nums = matcher.group(5).take(4)
                 var lon3 = nums.substring(0, 2)
                 var lat3 = nums.substring(2, 4)
-                println("georef2DMS nums $lon3 $lat3")
+                println("georef2DEG nums $lon3 $lat3")
                 var lon4 = lon1Val.toDouble() + lonMulti * (lon3.toDouble() / 60.0)
                 var lat4 = lat1Val.toDouble() + latMulti * (lat3.toDouble() / 60.0)
                 lon4 *= lonMulti
                 lat4 *= latMulti
-                println("georef2DMS nums2 $lon4 $lat4")
+                println("georef2DEG nums2 $lon4 $lat4")
                 retVal.Latitude = lat4
                 retVal.Longitude = lon4
 
             }
         }
-        println("georef2DMS - RETURN $retVal")
+        println("georef2DEG - RETURN $retVal")
         return retVal
 
     }
@@ -153,100 +153,15 @@ class Georef {
     }
 
     fun georef15ID(deg: Int): String {
-        if (0 <= deg && deg < 15) {
-            return "A"
-        }
-        if (15 <= deg && deg < 30) {
-            return "B"
-        }
-        if (30 <= deg && deg < 45) {
-            return "C"
-        }
-        if (45 <= deg && deg < 60) {
-            return "D"
-        }
-        if (60 <= deg && deg < 75) {
-            return "E"
-        }
-        if (75 <= deg && deg < 90) {
-            return "F"
-        }
-        if (90 <= deg && deg < 105) {
-            return "G"
-        }
-        if (105 <= deg && deg < 120) {
-            return "H"
-        }
-        if (120 <= deg && deg < 135) {
-            return "J"
-        }
-        if (135 <= deg && deg < 150) {
-            return "K"
-        }
-        if (150 <= deg && deg < 165) {
-            return "L"
-        }
-        if (165 <= deg && deg < 180) {
-            return "M"
-        }
-        if (180 <= deg && deg < 195) {
-            return "N"
-        }
-        if (195 <= deg && deg < 210) {
-            return "P"
-        }
-        if (210 <= deg && deg < 225) {
-            return "Q"
-        }
-        if (225 <= deg && deg < 240) {
-            return "R"
-        }
-        if (240 <= deg && deg < 255) {
-            return "S"
-        }
-        if (255 <= deg && deg < 270) {
-            return "T"
-        }
-        if (270 <= deg && deg < 285) {
-            return "U"
-        }
-        if (285 <= deg && deg < 300) {
-            return "V"
-        }
-        if (300 <= deg && deg < 315) {
-            return "W"
-        }
-        if (315 <= deg && deg < 330) {
-            return "X"
-        }
-        if (330 <= deg && deg < 345) {
-            return "Y"
-        }
-        if (345 <= deg && deg < 360) {
-            return "Z"
-        }
-
-        return ""
+        val letters01 = "ABCDEFGHJKLMNPQRSTUVWXYZ"
+        val char01 = letters01.toCharArray()[deg / 15]
+        return char01.toString()
     }
 
-    fun georef1515ID(deg: Int): String {
-        val letters: MutableMap<Int, String> = HashMap()
-        letters[0] = "A"
-        letters[1] = "B"
-        letters[2] = "C"
-        letters[3] = "D"
-        letters[4] = "E"
-        letters[5] = "F"
-        letters[6] = "G"
-        letters[7] = "H"
-        letters[8] = "J"
-        letters[9] = "K"
-        letters[10] = "L"
-        letters[11] = "M"
-        letters[12] = "N"
-        letters[13] = "P"
-        letters[14] = "Q"
-        val retVal = letters[deg]!!
+    fun georef1515ID(index: Int): String {
+        val letters01 = "ABCDEFGHJKLMNPQ"
+        val char01 = letters01.toCharArray()[index]
+        val retVal = char01.toString()
         return retVal
     }
 
